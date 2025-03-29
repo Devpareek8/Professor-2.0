a25398a03a07d7b0ad82aeaeee34782565733e22
Devpareek8 <165389457+Devpareek8@users.noreply.github.com>
2025-03-29 18:07:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,8 +21,8 @@ BATCH_FILES = {}
 join_db = JoinReqs
 
 @Client.on_message(filters.command("start") & filters.incoming)
- async def start(client, message):
-     await message.react(emoji="🔥")
+async def start(client, message):  # <- Yahan extra space hata diya
+    await message.react(emoji="🔥")  # <- Yeh bhi sahi indent hona chahiye
      if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
